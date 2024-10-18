e67c2dc0efc41cc8549e38cf682bc99cfeb526a0
Christian Minich <indyman402@gmail.com>
2024-10-18 10:04:46 -0400

    bump version and use materializable method

--- demo-dagster/azure_demo/jobs.py
+++ demo-dagster/azure_demo/jobs.py
@@ -7,8 +7,7 @@ workflow_a = dg.define_asset_job(
 
 workflow_b = dg.define_asset_job( 
     name="workflow_b",
-    selection=dg.AssetSelection.assets(["task_1__invoke_azure_function"]).downstream() -
-    dg.AssetSelection.assets(["dashboard", "hooli_example"])
+    selection=dg.AssetSelection.assets(["task_1__invoke_azure_function"]).materializable().downstream()
 )
 
 workflow_d = dg.define_asset_job(
--- demo-dagster/setup.py
+++ demo-dagster/setup.py
@@ -4,7 +4,7 @@ setup(
     name="azure_demo",
     packages=find_packages(exclude=["azure_demo_tests"]),
     install_requires=[
-        "dagster==1.8.7",
+        "dagster",
         "dagster-cloud",
         "requests",
         "azure-identity",
@@ -12,7 +12,7 @@ setup(
         "dagster-azure",
         "dagster-graphql",
         "dagster-databricks",
-        "dagster-powerbi==0.0.7" #@ git+https://github.com/dagster-io/dagster.git@dagster-powerbi/v0.0.8#egg=dagster-powerbi&subdirectory=python_modules/libraries/dagster-powerbi
+        "dagster-powerbi==0.0.10"
     ],
     extras_require={"dev": ["dagster-webserver", "pytest", "azure-functions"]},
 )
